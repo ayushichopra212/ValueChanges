6d1a569b66c7fe7de57c3ae8c52aab6fdab1adf3
Ayushi212 <ayushichopra212@gmail.com>
2021-08-20 03:16:17 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,30 +7,63 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  
-  title = 'ValueChanges';
-  feature = '';
+  miles: any;
+  ratePerMile: any;
+  totalRate: any;
+  initialRate = 0;
 
-  reactiveForm = new FormGroup({
-    firstname: new FormControl('', [Validators.required]),
-    address: new FormGroup({
-      city: new FormControl(),
-      street: new FormControl(),
-      pincode: new FormControl()
-    })
+  form = new FormGroup({
+    rate: new FormControl(''),
+    miles: new FormControl(''),
+    total: new FormControl('')
   })
+  // reactiveForm = new FormGroup({
+  //   firstname: new FormControl('', [Validators.required]),
+  //   address: new FormGroup({
+  //     city: new FormControl(),
+  //     street: new FormControl(),
+  //     pincode: new FormControl()
+  //   })
+  // })
+  //  onSave(){
+  //    console.log(this.form.value)
+  //  }
+  // onSubmit() {
+  //   console.log(this.reactiveForm.value);
+  // }
+  ngOnInit() {
+    this.form.get('miles').valueChanges.subscribe(data => {
+      // this.inputMiles = data;
+    })
 
-  onSubmit() {
-    console.log(this.reactiveForm.value);
-  }
-  ngOnInit(){
+    this.form.get('rate').valueChanges.subscribe(data => {
+      // this.inputRate = data;
+      this.multiplyInputs();
+    })
+    this.form.get('total').valueChanges.subscribe(data => {
+      this.totalRate = data;
+      this.divideInputs();
+    })
+
+
     // this.reactiveForm.get('firstname').valueChanges.subscribe(data =>{
     //   this.feature = data;
     //   console.log(data);
     // })
-    this.reactiveForm.valueChanges.subscribe(data=>{
-      
-      console.log(data);
-    })
+    // this.reactiveForm.valueChanges.subscribe(data=>{
+
+    //   console.log(data);
+    // })
+
+  }
+  multiplyInputs() {
+    this.ratePerMile = this.form.get('rate').value;
+    this.miles = this.form.get('miles').value;
+    this.initialRate = this.miles * this.ratePerMile;
+    console.log(this.initialRate)
+  }
+  divideInputs() {
+    this.ratePerMile = this.totalRate / this.miles;
+    // console.log(this.inputRate)
   }
 }
